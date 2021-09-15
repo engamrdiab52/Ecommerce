5f47636ebe6175e6d8d6dace4df8c01e8e49417a
engam <engamrdiab52@gmail.com>
2021-09-15 02:51:11 +0200

    dataBinding categories fragment

--- app/src/main/java/com/example/bags/Categories.kt
+++ app/src/main/java/com/example/bags/Categories.kt
@@ -5,16 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.bags.databinding.FragmentCategoriesBinding
 
 
 class Categories : Fragment() {
-
+    private lateinit var binding: FragmentCategoriesBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_categories, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_categories, container, false)
+        return binding.root
     }
 
 }
