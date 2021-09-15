66b50fdeeef9a95a0d693839e4b1b8280efdc10f
engam <engamrdiab52@gmail.com>
2021-09-15 03:41:48 +0200

    add home fragment

---
+++ app/src/main/java/com/example/bags/presentation/home/HomeFragment.kt
@@ -0,0 +1,24 @@
+package com.example.bags.presentation.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.bags.R
+import com.example.bags.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+}
