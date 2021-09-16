11b35583dce1efb55a9fcac91f26fb5ce8998249
engam <engamrdiab52@gmail.com>
2021-09-16 05:37:40 +0200

    validation added to login and credentials

--- app/src/main/java/com/example/bags/presentation/login/LoginFragment.kt
+++ app/src/main/java/com/example/bags/presentation/login/LoginFragment.kt
@@ -1,24 +1,40 @@
 package com.example.bags.presentation.login
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
+import com.example.bags.MainActivity.Companion.TAG
 import com.example.bags.R
 import com.example.bags.databinding.FragmentLoginBinding
+import com.wajahatkarim3.easyvalidation.core.view_ktx.validator
 
 class LoginFragment : Fragment() {
     private lateinit var binding: FragmentLoginBinding
+    private var validPassword: Boolean = false
+    private var validEmail: Boolean = false
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
         binding.buttonLogin.setOnClickListener {
-          //  findNavController().navigate(R.id.actio)
+            validateEmailField()
+            validatePasswordField()
+            if (validEmail && validPassword) {
+                val email = binding.editTextLoginEmail.text.toString()
+                val password = binding.editTextLoginPassword.text.toString()
+                Toast.makeText(requireContext(), "$email + $password", Toast.LENGTH_SHORT).show()
+            } else {
+                //may use singleLiveEvent
+                Toast.makeText(requireContext(), "** INVALID CREDENTIALS **", Toast.LENGTH_LONG)
+                    .show()
+            }
         }
         binding.tvLoginForgetPasswordClickable.setOnClickListener {
             findNavController().navigate(R.id.action_loginFragment_to_newCredentialsFragment)
@@ -28,6 +44,28 @@ class LoginFragment : Fragment() {
         }
         // Inflate the layout for this fragment
         return binding.root
+    }
+
+    private fun validatePasswordField() {
+        binding.editTextLoginPassword.validator().nonEmpty().addErrorCallback {
+            validPassword = false
+            binding.textLayoutLoginPassword.error = it
+            Log.d(TAG, it)
+        }.addSuccessCallback {
+            validPassword = true
+            binding.textLayoutLoginPassword.error = null
+        }.check()
+    }
+
+    private fun validateEmailField() {
+        binding.editTextLoginEmail.validator().nonEmpty().validEmail().addErrorCallback {
+            validEmail = false
+            binding.textLayoutLoginEmail.error = it
+            Log.d(TAG, it)
+        }.addSuccessCallback {
+            validEmail = true
+            binding.textLayoutLoginEmail.error = null
+        }.check()
     }
 
 }
--- app/src/main/java/com/example/bags/presentation/newCreadintials/NewCredentialsFragment.kt
+++ app/src/main/java/com/example/bags/presentation/newCreadintials/NewCredentialsFragment.kt
@@ -1,25 +1,68 @@
 package com.example.bags.presentation.newCreadintials
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
+import com.example.bags.MainActivity
+import com.example.bags.MainActivity.Companion.TAG
 import com.example.bags.R
 import com.example.bags.databinding.FragmentNewCredentialsBinding
+import com.wajahatkarim3.easyvalidation.core.view_ktx.validator
 
 class NewCredentialsFragment : Fragment() {
     private lateinit var binding: FragmentNewCredentialsBinding
+    var validPassword: Boolean = false
+    var validConfirmPassword: Boolean = false
+    var errorMessage: String = "Can't be empty!"
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_new_credentials, container, false)
+
+        binding.editTxtCredentialsPassword.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                s.toString().validator().nonEmpty().atleastOneNumber().atleastOneSpecialCharacters()
+                    .minLength(6).addErrorCallback {
+                        errorMessage =it
+                        binding.textLayoutCredentialsPassword.helperText = it
+                        validPassword = false
+                        Log.d(TAG, it)
+                    }.addSuccessCallback {
+                        validPassword = true
+                        Log.d(TAG, "SUCCESS")
+                    }.check()
+            }
+
+        })
+
         binding.btnNewCredentialsUpdate.setOnClickListener {
-            findNavController().navigate(R.id.action_newCredentialsFragment_to_loginFragment)
+            validateConfirmPasswordField()
+
+            if ( validConfirmPassword && validPassword) {
+                findNavController().navigate(R.id.action_newCredentialsFragment_to_loginFragment)
+            } else {
+                Toast.makeText(requireContext(), "** INVALID CREDENTIALS **", Toast.LENGTH_LONG)
+                    .show()
+            }
+
+
+            //findNavController().navigate(R.id.action_newCredentialsFragment_to_loginFragment)
         }
         binding.btnToHome.setOnClickListener {
             findNavController().navigate(R.id.action_global_nested_graph_home)
@@ -28,4 +71,34 @@ class NewCredentialsFragment : Fragment() {
         return binding.root
     }
 
+    private fun validateConfirmPasswordField() {
+        binding.editTxtCredentialsConfirmPassword.text.toString().validator().nonEmpty()
+            .addErrorCallback {
+                validConfirmPassword = false
+                binding.textLayoutCredentialsConfirmPassword.error = it
+                binding.textLayoutCredentialsPassword.error = errorMessage
+            }.addSuccessCallback {
+                val str1 = binding.editTxtCredentialsConfirmPassword.text.toString()
+                val str2 = binding.editTxtCredentialsPassword.text.toString()
+                val matched = str1 == str2
+                if (!matched) {
+                    binding.textLayoutCredentialsPassword.error = errorMessage
+                    binding.textLayoutCredentialsConfirmPassword.error =
+                        "Passwords do not match"
+                    validConfirmPassword = false
+                } else {
+                    if(!validPassword || str1.isEmpty()){
+                        binding.textLayoutCredentialsPassword.error = errorMessage
+                        binding.textLayoutCredentialsConfirmPassword.error =
+                            "Passwords are matched but not correct password"
+                        validConfirmPassword = false
+                    }else{
+                        binding.textLayoutCredentialsConfirmPassword.helperText =
+                            "Passwords are matched"
+                        validConfirmPassword = true
+                    }
+
+                }
+            }.check()
+    }
 }
